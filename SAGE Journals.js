029907cfe28090b5d70fc401b316461cadd3cff0
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-11-12 12:26:06 +0100

    local file changes last version sage journals

--- SAGE Journals.js
+++ SAGE Journals.js
@@ -9,11 +9,7 @@
 	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-<<<<<<< HEAD
 	"lastUpdated": "2020-11-11 16:50:27"
-=======
-	"lastUpdated": "2020-10-26 13:42:01"
->>>>>>> 13b3adeb8d1693389161cdeb9743603f8ac18b34
 }
 
 /*
@@ -53,11 +49,7 @@ function detectWeb(doc, url) {
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-<<<<<<< HEAD
 	var rows = ZU.xpath(doc, '//span[contains(@class, "art_title")]/a[contains(@href, "/doi/full/10.") or contains(@href, "/doi/abs/10.") or contains(@href, "/doi/pdf/10.")][1] | //a[contains(concat( " ", @class, " " ), concat( " ", "ref", " " )) and contains(concat( " ", @class, " " ), concat( " ", "nowrap", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "hlFld-Title", " " ))]');
-=======
-	let rows = ZU.xpath(doc, '//div[contains(@class, "art_title")]/a[contains(@href, "/doi/full/10.") or contains(@href, "/doi/abs/10.") or contains(@href, "/doi/pdf/10.")][1]');
->>>>>>> 13b3adeb8d1693389161cdeb9743603f8ac18b34
 	for (var i = 0; i < rows.length; i++) {
 		var href = rows[i].href;
 		var title = ZU.trimInternal(rows[i].textContent.replace(/Citation|ePub.*|Abstract/, ''));
@@ -99,14 +91,14 @@ function scrape(doc, url) {
 	var post = "doi=" + encodeURIComponent(doi) + "&include=abs&format=ris&direct=false&submit=Download+Citation";
 	var pdfurl = "//" + doc.location.host + "/doi/pdf/" + doi;
 	var articleType = ZU.xpath(doc, '//span[@class="ArticleType"]/span');
-	// Z.debug(pdfurl);
-	// Z.debug(post);
+	//Z.debug(pdfurl);
+	//Z.debug(post);
 	ZU.doPost(risURL, post, function (text) {
-		// The publication date is saved in DA and the date first
-		// appeared online is in Y1. Thus, we want to prefer DA over T1
-		// and will therefore simply delete the later in cases both
-		// dates are present.
-		// Z.debug(text);
+		//The publication date is saved in DA and the date first
+		//appeared online is in Y1. Thus, we want to prefer DA over T1
+		//and will therefore simply delete the later in cases both
+		//dates are present.
+		//Z.debug(text);
 		if (text.includes("DA  - ")) {
 			text = text.replace(/Y1\s{2}- .*\r?\n/, '');
 		} // Z.debug(text);
@@ -145,16 +137,10 @@ function scrape(doc, url) {
 				item.abstractNote = abstract;
 			}
 
-			// Only get attributes and skip introductory term Keywords
-			let tagentry = ZU.xpathText(doc, '//kwd-group[1] | //*[contains(concat( " ", @class, " " ), concat( " ", "hlFld-KeywordText", " " ))] \
-																  /a[@class="attributes"]');
-			// Make sure we keep the original approach
-			if (!tagentry)
-				tagentry = ZU.xpathText(doc, '//kwd-group[1] | //*[contains(concat( " ", @class, " " ), concat( " ", "hlFld-KeywordText", " " ))]');
+			var tagentry = ZU.xpathText(doc, '//kwd-group[1] | //*[contains(concat( " ", @class, " " ), concat( " ", "hlFld-KeywordText", " " ))]');
 			if (tagentry) {
 				item.tags = tagentry.replace(/.*Keywords/, ',').replace(/Mots-clés/, ',').split(",");
 			}
-
 			// ubtue: add tags "Book Review" if "Review Article"
 			if (articleType) {
 				for (let r of articleType) {
@@ -182,12 +168,7 @@ function scrape(doc, url) {
 					if (tags) item.tags = tags.map(n => n.textContent);
 				}
 			}
-<<<<<<< HEAD
 			
-=======
-
-			item.notes = [];
->>>>>>> 13b3adeb8d1693389161cdeb9743603f8ac18b34
 			item.language = ZU.xpathText(doc, '//meta[@name="dc.Language"]/@content');
 			item.attachments.push({
 				url: pdfurl,
@@ -199,207 +180,3 @@ function scrape(doc, url) {
 		translator.translate();
 	});
 }
-
-<<<<<<< HEAD
-=======
-/** BEGIN TEST CASES **/
-var testCases = [
-	{
-		"type": "web",
-		"url": "http://journals.sagepub.com/doi/abs/10.1177/1754073910380971",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"title": "Emotion and Regulation are One!",
-				"creators": [
-					{
-						"firstName": "Arvid",
-						"lastName": "Kappas",
-						"creatorType": "author"
-					}
-				],
-				"date": "January 1, 2011",
-				"DOI": "10.1177/1754073910380971",
-				"ISSN": "1754-0739",
-				"abstractNote": "Emotions are foremost self-regulating processes that permit rapid responses and adaptations to situations of personal concern. They have biological bases and are shaped ontogenetically via learning and experience. Many situations and events of personal concern are social in nature. Thus, social exchanges play an important role in learning about rules and norms that shape regulation processes. I argue that (a) emotions often are actively auto-regulating—the behavior implied by the emotional reaction bias to the eliciting event or situation modifies or terminates the situation; (b) certain emotion components are likely to habituate dynamically, modifying the emotional states; (c) emotions are typically intra- and interpersonal processes at the same time, and modulating forces at these different levels interact; (d) emotions are not just regulated—they regulate. Important conclusions of my arguments are that the scientific analysis of emotion should not exclude regulatory processes, and that effortful emotion regulation should be seen relative to a backdrop of auto-regulation and habituation, and not the ideal notion of a neutral baseline. For all practical purposes unregulated emotion is not a realistic concept.",
-				"issue": "1",
-				"journalAbbreviation": "Emotion Review",
-				"language": "en",
-				"libraryCatalog": "SAGE Journals",
-				"pages": "17-25",
-				"publicationTitle": "Emotion Review",
-				"url": "https://doi.org/10.1177/1754073910380971",
-				"volume": "3",
-				"attachments": [
-					{
-						"title": "SAGE PDF Full Text",
-						"mimeType": "application/pdf"
-					}
-				],
-				"tags": [
-					{
-						"tag": "emotion regulation"
-					},
-					{
-						"tag": "facial expression"
-					},
-					{
-						"tag": "facial feedback"
-					}
-				],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "http://journals.sagepub.com/toc/rera/86/3",
-		"items": "multiple"
-	},
-	{
-		"type": "web",
-		"url": "http://journals.sagepub.com/doi/full/10.1177/0954408914525387",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"title": "Brookfield powder flow tester – Results of round robin tests with CRM-116 limestone powder",
-				"creators": [
-					{
-						"firstName": "R. J.",
-						"lastName": "Berry",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "M. S. A.",
-						"lastName": "Bradley",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "R. G.",
-						"lastName": "McGregor",
-						"creatorType": "author"
-					}
-				],
-				"date": "August 1, 2015",
-				"DOI": "10.1177/0954408914525387",
-				"ISSN": "0954-4089",
-				"abstractNote": "A low cost powder flowability tester for industry has been developed at The Wolfson Centre for Bulk Solids Handling Technology, University of Greenwich in collaboration with Brookfield Engineering and four food manufacturers: Cadbury, Kerry Ingredients, GSK and United Biscuits. Anticipated uses of the tester are primarily for quality control and new product development, but it can also be used for storage vessel design., This paper presents the preliminary results from ‘round robin’ trials undertaken with the powder flow tester using the BCR limestone (CRM-116) standard test material. The mean flow properties have been compared to published data found in the literature for the other shear testers.",
-				"issue": "3",
-				"journalAbbreviation": "Proceedings of the Institution of Mechanical Engineers, Part E: Journal of Process Mechanical Engineering",
-				"libraryCatalog": "SAGE Journals",
-				"pages": "215-230",
-				"publicationTitle": "Proceedings of the Institution of Mechanical Engineers, Part E: Journal of Process Mechanical Engineering",
-				"url": "https://doi.org/10.1177/0954408914525387",
-				"volume": "229",
-				"attachments": [
-					{
-						"title": "SAGE PDF Full Text",
-						"mimeType": "application/pdf"
-					}
-				],
-				"tags": [
-					{
-						"tag": "Shear cell"
-					},
-					{
-						"tag": "BCR limestone powder (CRM-116)"
-					},
-					{
-						"tag": "flow function"
-					},
-					{
-						"tag": "characterizing powder flowability"
-					},
-					{
-						"tag": "reproducibility"
-					},
-					{
-						"tag": "Brookfield powder flow tester"
-					},
-					{
-						"tag": "Jenike shear cell"
-					},
-					{
-						"tag": "Schulze ring shear tester"
-					}
-				],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "http://journals.sagepub.com/action/doSearch?AllField=test",
-		"items": "multiple"
-	},
-	{
-		"type": "web",
-		"url": "http://journals.sagepub.com/doi/full/10.1177/1541204015581389",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"title": "Moffitt’s Developmental Taxonomy and Gang Membership: An Alternative Test of the Snares Hypothesis",
-				"creators": [
-					{
-						"firstName": "Melissa A.",
-						"lastName": "Petkovsek",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Brian B.",
-						"lastName": "Boutwell",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "J. C.",
-						"lastName": "Barnes",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Kevin M.",
-						"lastName": "Beaver",
-						"creatorType": "author"
-					}
-				],
-				"date": "October 1, 2016",
-				"DOI": "10.1177/1541204015581389",
-				"ISSN": "1541-2040",
-				"abstractNote": "Moffitt’s taxonomy remains an influential theoretical framework within criminology. Despite much empirical scrutiny, comparatively less time has been spent testing the snares component of Moffitt’s work. Specifically, are there factors that might engender continued criminal involvement for individuals otherwise likely to desist? The current study tested whether gang membership increased the odds of contact with the justice system for each of the offender groups specified in Moffitt’s original developmental taxonomy. Our findings provided little evidence that gang membership increased the odds of either adolescence-limited or life-course persistent offenders being processed through the criminal justice system. Moving forward, scholars may wish to shift attention to alternative variables—beyond gang membership—when testing the snares hypothesis.",
-				"issue": "4",
-				"journalAbbreviation": "Youth Violence and Juvenile Justice",
-				"libraryCatalog": "SAGE Journals",
-				"pages": "335-349",
-				"publicationTitle": "Youth Violence and Juvenile Justice",
-				"shortTitle": "Moffitt’s Developmental Taxonomy and Gang Membership",
-				"url": "https://doi.org/10.1177/1541204015581389",
-				"volume": "14",
-				"attachments": [
-					{
-						"title": "SAGE PDF Full Text",
-						"mimeType": "application/pdf"
-					}
-				],
-				"tags": [
-					{
-						"tag": "Moffitt’s developmental taxonomy"
-					},
-					{
-						"tag": "gang membership"
-					},
-					{
-						"tag": "snares"
-					},
-					{
-						"tag": "delinquency"
-					}
-				],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	}
-]
-/** END TEST CASES **/
->>>>>>> 13b3adeb8d1693389161cdeb9743603f8ac18b34
